ffdf716fdf9518f2058576d47a51d7dabd32bcf4
jennyuuu010 <115968428+jennyuuu010@users.noreply.github.com>
2023-06-28 17:24:33 +0900

    새로 프로젝트 생성

---
+++ Source/YSnakeProject/YSnakeProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "YSnakeProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, YSnakeProject, "YSnakeProject" );
---
+++ Source/YSnakeProject/YSnakeProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "YSnakeProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YSNAKEPROJECT_API AYSnakeProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
